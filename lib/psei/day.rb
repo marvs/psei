6cabeff306da1394568110910e0d67e739554559
Marvin Baltazar <marvin.baltazar@gmail.com>
2016-07-09 01:03:03 +0800

    Psei::Day values method and securities hash

--- lib/psei/day.rb
+++ lib/psei/day.rb
@@ -1,20 +1,38 @@
 class Psei::Day
   SOURCE_URL = "http://pse.com.ph/stockMarket/home.html?method=getSecuritiesAndIndicesForPublic&ajax=true"
+  HEADING_ALIAS = "Stock Update As of"
+  INDEX_ALIASES = ["PSEi", "All Shares", "Financials", "Industrial", "Holding Firms", "Property", "Services", "Mining and Oil"].freeze
   attr_reader :parsed
   
   def initialize
     @parsed = Psei::Parser.new(SOURCE_URL).process
   end
   
+  # Returns a Hash of last traded prices per security
+  def values
+    securities_hash
+  end
+  
+  # Returns the last traded price of a specific security
   def value symbol
-    stock = find_by_symbol symbol
-    stock['lastTradedPrice'].to_f
+    #stock = find_by_symbol symbol
+    #stock['lastTradedPrice'].to_f
+    values[symbol.to_s]
   end
   
   private
   
   def find_by_symbol symbol
-    @parsed.detect{ |s| s['securitySymbol'] == symbol.to_s }
+    securities = securities_filter
+    securities.detect{ |s| s['securitySymbol'] == symbol.to_s }
+  end
+  
+  def securities_filter
+    @parsed.reject{|x| (INDEX_ALIASES).include?(x['securityAlias']) || x['securitySymbol'] == HEADING_ALIAS}
+  end
+  
+  def securities_hash
+    @sec_hash ||= Hash[securities_filter.collect{|item| [item['securitySymbol'], item['lastTradedPrice'].to_f] }]
   end
   
 end
